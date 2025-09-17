1400d1aaf075d7a4ba8f7615ebe26d410e944848
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-18 00:17:52 +0200

    create database table object;

---
+++ app/src/main/java/com/example/stockmarketappproject/data/local/entity/CompanyListingEntity.kt
@@ -0,0 +1,13 @@
+package com.example.stockmarketappproject.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class CompanyListingEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val symbol: String,
+    val exchange: String,
+)

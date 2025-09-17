ee6d31663c0bd5c02858fd40aa7494dc5dee1dd4
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-18 01:04:00 +0200

    create database;

---
+++ app/src/main/java/com/example/stockmarketappproject/data/local/database/StockDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.stockmarketappproject.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.stockmarketappproject.data.local.dao.StockDao
+import com.example.stockmarketappproject.data.local.entity.CompanyListingEntity
+
+@Database(
+    entities = [CompanyListingEntity::class],
+    version = 1
+)
+abstract class StockDatabase : RoomDatabase() {
+    abstract val stockDao: StockDao
+}

c897a9f462299a7a9e0a009707c9dab6e8730c98
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-18 14:07:09 +0200

    create implementation for repository (wip);

---
+++ app/src/main/java/com/example/stockmarketappproject/data/repository/StockRepositoryImpl.kt
@@ -0,0 +1,63 @@
+package com.example.stockmarketappproject.data.repository
+
+import com.example.stockmarketappproject.data.local.dao.StockDao
+import com.example.stockmarketappproject.data.mappers.StockMapper
+import com.example.stockmarketappproject.data.remote.api.StockApi
+import com.example.stockmarketappproject.domain.model.CompanyListingDomain
+import com.example.stockmarketappproject.domain.repository.StockRepository
+import com.example.stockmarketappproject.utils.model.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.transform
+import okio.IOException
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class StockRepositoryImpl @Inject constructor(
+    private val stockApi: StockApi,
+    private val stockDao: StockDao,
+    private val stockMapper: StockMapper
+) : StockRepository {
+
+//    override suspend fun getCompanyListing(query: String): Flow<Resource<List<CompanyListingDomain>>> {
+//        return flow {
+//            try {
+//                val result = stockDao.searchCompanyListing(query)
+//                    .map { companyListingEntity ->
+//                        with(stockMapper) {
+//                            companyListingEntity.toCompanyListingData()
+//                        }
+//                    }
+//                emit(Resource.Success(result))
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//                emit(Resource.Error(message = e.localizedMessage))
+//            }
+//        }
+//    }
+
+    override fun getCompanyListing(query: String): Flow<Resource<List<CompanyListingDomain>>> =
+        stockDao.searchCompanyListing(query).transform { companyListingEntities ->
+            try {
+                val result = with(stockMapper) {
+                    companyListingEntities.map { entity -> entity.toCompanyListingData() }
+                }
+                Resource.Success(data = result)
+            } catch (e: Exception) {
+                Resource.Error(e.localizedMessage)
+            }
+        }
+
+    override suspend fun fetchCompanyListing(): Resource<CompanyListingDomain> {
+        return try {
+            val response = stockApi.getListings()
+            // TODO: create separate class for parsing
+            Resource.Success()
+        } catch (ioe: IOException) {
+            Resource.Error(ioe.localizedMessage)
+        } catch (httpe: HttpException) {
+            Resource.Error(httpe.localizedMessage)
+        }
+    }
+
+
+}

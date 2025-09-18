efa383042012b6ef94bf530d101a0bec015c833c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-18 15:42:01 +0200

    provide implementation of csv reader;

---
+++ app/src/main/java/com/example/stockmarketappproject/data/parser/CsvParserImpl.kt
@@ -0,0 +1,37 @@
+package com.example.stockmarketappproject.data.parser
+
+import com.example.stockmarketappproject.data.model.CompanyListingData
+import com.opencsv.CSVReader
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.io.InputStreamReader
+import javax.inject.Inject
+
+
+class CsvParserImpl @Inject constructor() : CsvParser<CompanyListingData> {
+
+    override suspend fun parse(stream: InputStream): List<CompanyListingData> {
+        val reader = CSVReader(InputStreamReader(stream))
+        return withContext(Dispatchers.IO) {
+            reader.use {
+                it.readAll()
+                    //Skip headers
+                    .drop(1)
+                    .mapNotNull { row ->
+                        val symbol = row.getOrNull(0)
+                        val name = row.getOrNull(1)
+                        val exchange = row.getOrNull(2)
+
+                        CompanyListingData(
+                            //Return null, then map will skip this entry
+                            symbol = symbol ?: return@mapNotNull null,
+                            name = name ?: return@mapNotNull null,
+                            exchange = exchange ?: return@mapNotNull null
+                        )
+                    }
+            }
+        }
+    }
+
+}

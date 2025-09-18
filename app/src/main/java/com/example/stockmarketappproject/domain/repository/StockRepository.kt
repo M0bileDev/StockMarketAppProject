b5e1f5726610927dc824e8b2d2dceef8dd2409dc
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-18 14:06:41 +0200

    create abstraction for repository;

---
+++ app/src/main/java/com/example/stockmarketappproject/domain/repository/StockRepository.kt
@@ -0,0 +1,14 @@
+package com.example.stockmarketappproject.domain.repository
+
+import com.example.stockmarketappproject.domain.model.CompanyListingDomain
+import com.example.stockmarketappproject.utils.model.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface StockRepository {
+
+    fun getCompanyListing(
+        query: String
+    ): Flow<Resource<List<CompanyListingDomain>>>
+
+    suspend fun fetchCompanyListing(): Resource<CompanyListingDomain>
+}

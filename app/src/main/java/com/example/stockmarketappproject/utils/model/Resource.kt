eb46f62d5c110dad4efc0a8e5125ff82776bfb00
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-18 12:16:38 +0200

    create utility resource class;

---
+++ app/src/main/java/com/example/stockmarketappproject/utils/model/Resource.kt
@@ -0,0 +1,6 @@
+package com.example.stockmarketappproject.utils.model
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T?) : Resource<T>(data)
+    class Error<T>(message: String?, data: T?) : Resource<T>(data, message)
+}

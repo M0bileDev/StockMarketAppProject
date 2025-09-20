d06212533fd99f7a299fd409d840e372d15fecb7
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-20 02:13:42 +0200

    create view model;

---
+++ app/src/main/java/com/example/stockmarketappproject/presentation/screen/companylisting/CompanyListingViewModel.kt
@@ -0,0 +1,101 @@
+package com.example.stockmarketappproject.presentation.screen.companylisting
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.example.stockmarketappproject.data.repository.DefaultStockRepository
+import com.example.stockmarketappproject.presentation.model.CompanyListingEvent
+import com.example.stockmarketappproject.presentation.model.CompanyListingState
+import com.example.stockmarketappproject.presentation.model.ViewModelEvents
+import com.example.stockmarketappproject.utils.model.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import kotlin.properties.Delegates
+
+const val TAG = "CompanyListingViewModel"
+
+@HiltViewModel
+class CompanyListingViewModel @Inject constructor(
+    private val stockRepository: DefaultStockRepository
+) : ViewModel() {
+
+    init {
+        fetchCompanies()
+    }
+
+    private var searchJob: Job? = null
+    private var fetchJob: Job? = null
+    private val searchScope = CoroutineScope(Dispatchers.IO)
+    private val fetchScope = CoroutineScope(Dispatchers.IO)
+
+    private var search by Delegates.observable("") { prop, old, new ->
+        with(searchScope) {
+            searchJob?.cancel()
+            searchJob = launch {
+                if (!isActive) return@launch
+                stockRepository.getCompanyListing(new).collectLatest { resource ->
+                    when (resource) {
+                        is Resource.Error -> TODO()
+                        is Resource.Success -> TODO()
+                    }
+                }
+            }
+        }
+    }
+
+    private val _state = MutableStateFlow(CompanyListingState.createDefault())
+    val state get() = _state.asStateFlow()
+
+    private val _viewModelEvent = MutableSharedFlow<ViewModelEvents>()
+    val event get() = _viewModelEvent.asSharedFlow()
+
+    private fun fetchCompanies() = with(fetchScope) {
+        fetchJob?.cancel()
+        fetchJob = launch {
+            if (!isActive) return@launch
+
+            _state.value = _state.value.copy(isLoading = true)
+
+            stockRepository.fetchCompanyListing().collectLatest { resource ->
+                when (resource) {
+                    is Resource.Error -> {
+                        _viewModelEvent.emit(ViewModelEvents.NetworkError)
+                    }
+
+                    is Resource.Success -> {
+                        Log.d(TAG, "Data has been fetched successfully...")
+                    }
+                }
+
+                _state.value = _state.value.copy(isLoading = false)
+            }
+        }
+
+    }
+
+    fun onEvent(companyListingEvent: CompanyListingEvent) {
+        when (companyListingEvent) {
+            is CompanyListingEvent.OnNavigate -> TODO()
+            is CompanyListingEvent.OnSearchQueryChange -> {
+                search = companyListingEvent.query
+            }
+
+            is CompanyListingEvent.OnRefresh -> ::fetchCompanies
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        searchJob?.cancel()
+    }
+
+}

ebe4fa26148df3908396e83c1aa37c9a80fd25fc
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-21 02:29:53 +0200

    create default representation of parser;

---
+++ app/src/main/java/com/example/stockmarketappproject/data/parser/DefaultCsvParser.kt
@@ -0,0 +1,5 @@
+package com.example.stockmarketappproject.data.parser
+
+import com.example.stockmarketappproject.data.model.CompanyListingData
+
+interface DefaultCsvParser : CsvParser<CompanyListingData>

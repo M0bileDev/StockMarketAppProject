6b2c6975405dd8ed54a044789ff45791401d5868
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-21 02:29:28 +0200

    create default representation of mapper;

---
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/DefaultStockDataMapper.kt
@@ -0,0 +1,6 @@
+package com.example.stockmarketappproject.data.mappers
+
+import com.example.stockmarketappproject.data.local.entity.CompanyListingEntity
+import com.example.stockmarketappproject.data.model.CompanyListingData
+
+interface DefaultStockDataMapper : StockDataMapper<CompanyListingEntity, CompanyListingData>

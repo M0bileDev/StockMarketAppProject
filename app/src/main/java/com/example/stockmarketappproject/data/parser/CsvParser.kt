f8d3e7510189f6a8a92bebd7170ef982566b40a2
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-21 02:46:53 +0200

    update type argument to depend on abstraction;

--- app/src/main/java/com/example/stockmarketappproject/data/parser/CsvParser.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/parser/CsvParser.kt
@@ -1,8 +1,9 @@
 package com.example.stockmarketappproject.data.parser
 
+import com.example.stockmarketappproject.data.model.DataModel
 import com.example.stockmarketappproject.domain.model.CompanyListingDomain
 import java.io.InputStream
 
-interface CsvParser<out T: CompanyListingDomain> {
+interface CsvParser<out T : DataModel> {
     suspend fun parse(stream: InputStream): List<T>
 }

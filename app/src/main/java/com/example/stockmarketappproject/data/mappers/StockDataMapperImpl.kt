6a0d9142db07be937b317ae17b5946900a96281c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-21 02:28:01 +0200

    rename mapper; provide di annotation and constructor;

--- app/src/main/java/com/example/stockmarketappproject/data/mappers/StockDataMapperImpl.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/StockDataMapperImpl.kt
@@ -2,8 +2,9 @@ package com.example.stockmarketappproject.data.mappers
 
 import com.example.stockmarketappproject.data.local.entity.CompanyListingEntity
 import com.example.stockmarketappproject.data.model.CompanyListingData
+import javax.inject.Inject
 
-class StockMapperImpl : StockMapper {
+class StockDataMapperImpl @Inject constructor() : DefaultStockDataMapper {
     override fun CompanyListingEntity.toCompanyListingData(): CompanyListingData {
         return CompanyListingData(name, symbol, exchange)
     }

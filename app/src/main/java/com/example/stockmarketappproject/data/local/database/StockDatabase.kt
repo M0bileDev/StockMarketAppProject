fe2648d39c3a106b278977e09b33ae2f4cfe9120
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 14:48:57 +0200

    update database;

--- app/src/main/java/com/example/stockmarketappproject/data/local/database/StockDatabase.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/local/database/StockDatabase.kt
@@ -2,14 +2,23 @@ package com.example.stockmarketappproject.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.stockmarketappproject.data.local.dao.StockDao
+import com.example.stockmarketappproject.data.local.dao.InfoDao
+import com.example.stockmarketappproject.data.local.dao.IntradayDao
+import com.example.stockmarketappproject.data.local.dao.ListingDao
+import com.example.stockmarketappproject.data.local.entity.CompanyInfoEntity
+import com.example.stockmarketappproject.data.local.entity.CompanyIntradayInfoEntity
 import com.example.stockmarketappproject.data.local.entity.CompanyListingEntity
 
 @Database(
-    entities = [CompanyListingEntity::class],
-    version = 1,
+    entities = [
+        CompanyListingEntity::class,
+        CompanyIntradayInfoEntity::class,
+        CompanyInfoEntity::class],
+    version = 2,
     exportSchema = false
 )
 abstract class StockDatabase : RoomDatabase() {
-    abstract val stockDao: StockDao
+    abstract val listingDao: ListingDao
+    abstract val intradayDao: IntradayDao
+    abstract val infoDao: InfoDao
 }

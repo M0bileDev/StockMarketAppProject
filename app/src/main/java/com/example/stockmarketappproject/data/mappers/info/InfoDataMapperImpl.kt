a23112d7b52675168301d031d4a6c9f74d0378ae
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 12:25:18 +0200

    add di annotation;

--- app/src/main/java/com/example/stockmarketappproject/data/mappers/info/InfoDataMapperImpl.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/info/InfoDataMapperImpl.kt
@@ -2,8 +2,9 @@ package com.example.stockmarketappproject.data.mappers.info
 
 import com.example.stockmarketappproject.data.model.info.CompanyInfoData
 import com.example.stockmarketappproject.data.remote.model.dto.info.CompanyInfoDto
+import javax.inject.Inject
 
-class InfoDataMapperImpl : DefaultInfoDataMapper {
+class InfoDataMapperImpl @Inject constructor() : DefaultInfoDataMapper {
     override fun CompanyInfoDto.toCompanyInfoData(): CompanyInfoData {
         return CompanyInfoData(
             symbol,

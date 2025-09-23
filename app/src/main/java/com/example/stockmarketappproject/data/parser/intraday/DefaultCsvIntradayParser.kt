7376fa971d622835c0ac37050612511ef7d3eb3c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 15:28:29 +0200

    introduce default csv intraday parser;

---
+++ app/src/main/java/com/example/stockmarketappproject/data/parser/intraday/DefaultCsvIntradayParser.kt
@@ -0,0 +1,6 @@
+package com.example.stockmarketappproject.data.parser.intraday
+
+import com.example.stockmarketappproject.data.model.intraday.CompanyIntradayInfoData
+import com.example.stockmarketappproject.data.parser.CsvParser
+
+interface DefaultCsvIntradayParser : CsvParser<CompanyIntradayInfoData>

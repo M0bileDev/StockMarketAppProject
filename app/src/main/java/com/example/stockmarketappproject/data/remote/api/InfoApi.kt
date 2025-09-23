c75f837962905e8077d4dced79eec56452f021bf
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 14:43:06 +0200

    introduce company info api;

---
+++ app/src/main/java/com/example/stockmarketappproject/data/remote/api/InfoApi.kt
@@ -0,0 +1,13 @@
+package com.example.stockmarketappproject.data.remote.api
+
+import com.example.stockmarketappproject.data.remote.model.dto.info.CompanyInfoDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface InfoApi {
+
+    @GET("query?function=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("symbol") symbol: String
+    ): CompanyInfoDto
+}

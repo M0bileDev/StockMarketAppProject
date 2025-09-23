c0016b6760e69cfdf1d33bea723256693f939d9a
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 14:44:43 +0200

    introduce company intraday repo;

---
+++ app/src/main/java/com/example/stockmarketappproject/domain/repository/IntradayRepository.kt
@@ -0,0 +1,16 @@
+package com.example.stockmarketappproject.domain.repository
+
+import com.example.stockmarketappproject.domain.model.DomainModel
+import com.example.stockmarketappproject.utils.model.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface IntradayRepository<Domain : DomainModel> {
+
+    fun getIntradayInfo(
+        symbol: String
+    ): Flow<Resource<List<Domain>>>
+
+    fun fetchIntradayInfo(
+        symbol: String
+    ): Flow<Resource<List<Domain>>>
+}

5c4cf10e21bcb04794cf6c5caebafebd04a78762
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 02:21:55 +0200

    package update;

--- app/src/main/java/com/example/stockmarketappproject/data/mappers/listing/DefaultListingDataMapper.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/listing/DefaultListingDataMapper.kt
@@ -1,6 +1,6 @@
 package com.example.stockmarketappproject.data.mappers.listing
 
 import com.example.stockmarketappproject.data.local.entity.CompanyListingEntity
-import com.example.stockmarketappproject.data.model.CompanyListingData
+import com.example.stockmarketappproject.data.model.listing.CompanyListingData
 
 interface DefaultListingDataMapper : ListingDataMapper<CompanyListingEntity, CompanyListingData>

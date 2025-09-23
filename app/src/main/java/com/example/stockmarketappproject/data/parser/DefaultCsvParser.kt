539f890f11f005e848653c566b229290a8ef9b8c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 14:57:09 +0200

    update package;

--- app/src/main/java/com/example/stockmarketappproject/data/parser/DefaultCsvParser.kt
+++
@@ -1,5 +0,0 @@
-package com.example.stockmarketappproject.data.parser
-
-import com.example.stockmarketappproject.data.model.listing.CompanyListingData
-
-interface DefaultCsvParser : CsvParser<CompanyListingData>
---
+++ app/src/main/java/com/example/stockmarketappproject/data/parser/listing/DefaultCsvListingParser.kt
@@ -0,0 +1,6 @@
+package com.example.stockmarketappproject.data.parser.listing
+
+import com.example.stockmarketappproject.data.model.listing.CompanyListingData
+import com.example.stockmarketappproject.data.parser.CsvParser
+
+interface DefaultCsvListingParser : CsvParser<CompanyListingData>

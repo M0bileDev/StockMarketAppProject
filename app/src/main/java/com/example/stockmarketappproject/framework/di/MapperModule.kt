87fbd67d76e667b19cdce7712a9421b337cac76f
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 12:25:50 +0200

    provide binders for mappers;

--- app/src/main/java/com/example/stockmarketappproject/framework/di/MapperModule.kt
+++ app/src/main/java/com/example/stockmarketappproject/framework/di/MapperModule.kt
@@ -1,5 +1,9 @@
 package com.example.stockmarketappproject.framework.di
 
+import com.example.stockmarketappproject.data.mappers.info.DefaultInfoDataMapper
+import com.example.stockmarketappproject.data.mappers.info.InfoDataMapperImpl
+import com.example.stockmarketappproject.data.mappers.intraday.DefaultIntradayDataMapper
+import com.example.stockmarketappproject.data.mappers.intraday.IntradayDataMapperImpl
 import com.example.stockmarketappproject.data.mappers.listing.DefaultListingDataMapper
 import com.example.stockmarketappproject.data.mappers.listing.ListingDataMapperImpl
 import com.example.stockmarketappproject.presentation.mapper.DefaultStockPresentationMapper
@@ -18,4 +22,10 @@ abstract class MapperModule {
 
     @[Binds Singleton]
     abstract fun bindStockPresentationMapper(stockPresentationMapperImpl: StockPresentationMapperImpl): DefaultStockPresentationMapper
+
+    @[Binds Singleton]
+    abstract fun bindInfoDataMapper(infoDataMapperImpl: InfoDataMapperImpl): DefaultInfoDataMapper
+
+    @[Binds Singleton]
+    abstract fun bindIntradayDataMapper(intradayDataMapperImpl: IntradayDataMapperImpl): DefaultIntradayDataMapper
 }

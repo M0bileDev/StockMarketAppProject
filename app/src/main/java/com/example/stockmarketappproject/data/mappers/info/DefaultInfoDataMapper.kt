e7cbbda727e17ddba56d5ba9db1a80a5cfab0b12
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 02:33:05 +0200

    provide default info data mapper;

---
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/info/DefaultInfoDataMapper.kt
@@ -0,0 +1,6 @@
+package com.example.stockmarketappproject.data.mappers.info
+
+import com.example.stockmarketappproject.data.model.info.CompanyInfoData
+import com.example.stockmarketappproject.data.remote.model.dto.info.CompanyInfoDto
+
+interface DefaultInfoDataMapper : InfoDataMapper<CompanyInfoDto, CompanyInfoData>

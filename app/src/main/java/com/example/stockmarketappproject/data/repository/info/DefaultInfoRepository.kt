d66f09b4b263083860e8389a11eb01c77a570391
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 14:42:28 +0200

    provide default info repo;

---
+++ app/src/main/java/com/example/stockmarketappproject/data/repository/info/DefaultInfoRepository.kt
@@ -0,0 +1,6 @@
+package com.example.stockmarketappproject.data.repository.info
+
+import com.example.stockmarketappproject.data.model.info.CompanyInfoData
+import com.example.stockmarketappproject.domain.repository.InfoRepository
+
+interface DefaultInfoRepository : InfoRepository<CompanyInfoData>

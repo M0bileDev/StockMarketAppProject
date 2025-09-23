51d6fba4cd2adbe710404c78ec7eb82894fdf5d0
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 14:42:50 +0200

    rename default repo;

--- app/src/main/java/com/example/stockmarketappproject/data/repository/DefaultStockRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.stockmarketappproject.data.repository
-
-import com.example.stockmarketappproject.data.model.listing.CompanyListingData
-import com.example.stockmarketappproject.domain.repository.StockRepository
-
-interface DefaultStockRepository : StockRepository<CompanyListingData>
---
+++ app/src/main/java/com/example/stockmarketappproject/data/repository/listing/DefaultListingRepository.kt
@@ -0,0 +1,6 @@
+package com.example.stockmarketappproject.data.repository.listing
+
+import com.example.stockmarketappproject.data.model.listing.CompanyListingData
+import com.example.stockmarketappproject.domain.repository.ListingRepository
+
+interface DefaultListingRepository : ListingRepository<CompanyListingData>

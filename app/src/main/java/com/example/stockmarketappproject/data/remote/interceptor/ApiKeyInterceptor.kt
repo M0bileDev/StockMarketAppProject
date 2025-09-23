d384c0e743f49aab2514ff5cb80af84269b47396
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 13:01:51 +0200

    introduce api key interceptor (centralized place to provide api key to every request);

---
+++ app/src/main/java/com/example/stockmarketappproject/data/remote/interceptor/ApiKeyInterceptor.kt
@@ -0,0 +1,25 @@
+package com.example.stockmarketappproject.data.remote.interceptor
+
+import com.example.stockmarketappproject.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class ApiKeyInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val originalUrl = originalRequest.url
+
+        //provide api key
+        val newUrl = originalUrl
+            .newBuilder()
+            .addQueryParameter("apiKey", BuildConfig.API_KEY)
+            .build()
+
+        val newRequest = originalRequest
+            .newBuilder()
+            .url(newUrl)
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+}

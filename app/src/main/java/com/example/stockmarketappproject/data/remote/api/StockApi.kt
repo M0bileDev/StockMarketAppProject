45e780f89b4e14fc3f0153b3c3fcb3dabd2e4727
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 13:03:17 +0200

    remove hardcoded api key query param;

--- app/src/main/java/com/example/stockmarketappproject/data/remote/api/StockApi.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/remote/api/StockApi.kt
@@ -8,15 +8,15 @@ import retrofit2.http.Query
 
 interface StockApi {
 
-    @GET("query?function=LISTING_STATUS&apikey=${BuildConfig.API_KEY}")
+    @GET("query?function=LISTING_STATUS")
     suspend fun getListings(): ResponseBody
 
-    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv&apikey=${BuildConfig.API_KEY}")
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
     suspend fun getIntradayInfo(
         @Query("symbol") symbol: String
     ): ResponseBody
 
-    @GET("query?function=OVERVIEW&apikey=${BuildConfig.API_KEY}")
+    @GET("query?function=OVERVIEW")
     suspend fun getCompanyInfo(
         @Query("symbol") symbol: String
     ): CompanyInfoDto

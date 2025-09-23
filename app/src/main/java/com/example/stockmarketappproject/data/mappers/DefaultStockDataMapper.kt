33c92090b7a93c5baaff9104e88a9a93520e1f00
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 02:04:50 +0200

    update name;

--- app/src/main/java/com/example/stockmarketappproject/data/mappers/DefaultStockDataMapper.kt
+++
@@ -1,6 +0,0 @@
-package com.example.stockmarketappproject.data.mappers
-
-import com.example.stockmarketappproject.data.local.entity.CompanyListingEntity
-import com.example.stockmarketappproject.data.model.CompanyListingData
-
-interface DefaultStockDataMapper : StockDataMapper<CompanyListingEntity, CompanyListingData>
---
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/listing/DefaultListingDataMapper.kt
@@ -0,0 +1,6 @@
+package com.example.stockmarketappproject.data.mappers.listing
+
+import com.example.stockmarketappproject.data.local.entity.CompanyListingEntity
+import com.example.stockmarketappproject.data.model.CompanyListingData
+
+interface DefaultListingDataMapper : ListingDataMapper<CompanyListingEntity, CompanyListingData>

eac2ef656315cbb247f60b12cdf705204002212c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-24 01:18:02 +0200

    implement intraday repo;

--- app/src/main/java/com/example/stockmarketappproject/data/repository/intraday/IntradayRepositoryImpl.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/repository/intraday/IntradayRepositoryImpl.kt
@@ -1,17 +1,52 @@
 package com.example.stockmarketappproject.data.repository.intraday
 
+import com.example.stockmarketappproject.data.local.dao.IntradayDao
+import com.example.stockmarketappproject.data.mappers.intraday.DefaultIntradayDataMapper
 import com.example.stockmarketappproject.data.model.intraday.CompanyIntradayInfoData
+import com.example.stockmarketappproject.data.parser.intraday.DefaultCsvIntradayParser
+import com.example.stockmarketappproject.data.remote.api.IntradayApi
 import com.example.stockmarketappproject.utils.model.Resource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okio.IOException
+import retrofit2.HttpException
+import javax.inject.Inject
 
-class IntradayRepositoryImpl : DefaultIntradayRepository{
-    
-    override fun getIntradayInfo(symbol: String): Flow<Resource<List<CompanyIntradayInfoData>>> {
+class IntradayRepositoryImpl @Inject constructor(
+    private val intradayApi: IntradayApi,
+    private val intradayDao: IntradayDao,
+    private val defaultIntradayDataMapper: DefaultIntradayDataMapper,
+    private val defaultCsvIntradayParser: DefaultCsvIntradayParser
+) : DefaultIntradayRepository {
+
+    override fun getIntradayInfo(name: String): Flow<Resource<List<CompanyIntradayInfoData>>> {
         TODO("Not yet implemented")
     }
 
-    override fun fetchIntradayInfo(symbol: String): Flow<Resource<List<CompanyIntradayInfoData>>> {
-        TODO("Not yet implemented")
+    override fun fetchIntradayInfo(name: String): Flow<Resource<List<CompanyIntradayInfoData>>> {
+        return flow {
+            try {
+                val response = intradayApi.getIntradayInfo(name)
+                val data = defaultCsvIntradayParser.parse(response.byteStream())
+                val result = with(defaultIntradayDataMapper) {
+                    data.map { companyIntradayInfoData ->
+                        companyIntradayInfoData.toCompanyIntradayInfoEntity(
+                            name
+                        )
+                    }
+                }
+                if (result.isEmpty()) throw IllegalStateException("Data is empty")
+
+                intradayDao.deleteCompanyIntradayInfo(name)
+                intradayDao.insertCompanyIntradayInfo(result)
+            } catch (ise: IllegalStateException) {
+                emit(Resource.Error(ise.localizedMessage))
+            } catch (ioe: IOException) {
+                emit(Resource.Error(ioe.localizedMessage))
+            } catch (httpe: HttpException) {
+                emit(Resource.Error(httpe.localizedMessage))
+            }
+        }
     }
 
 }

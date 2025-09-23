9516c430d34b3a0d92fd6f4286e1c6dbacb3fcdc
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-24 01:19:55 +0200

    update default mapper;

--- app/src/main/java/com/example/stockmarketappproject/data/mappers/intraday/DefaultIntradayDataMapper.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/intraday/DefaultIntradayDataMapper.kt
@@ -1,7 +1,8 @@
 package com.example.stockmarketappproject.data.mappers.intraday
 
+import com.example.stockmarketappproject.data.local.entity.CompanyIntradayInfoEntity
 import com.example.stockmarketappproject.data.model.intraday.CompanyIntradayInfoData
 import com.example.stockmarketappproject.data.remote.model.dto.intraday.CompanyIntradayInfoDto
 
 interface DefaultIntradayDataMapper :
-    IntradayDataMapper<CompanyIntradayInfoDto, CompanyIntradayInfoData>
+    IntradayDataMapper<CompanyIntradayInfoDto, CompanyIntradayInfoData, CompanyIntradayInfoEntity>

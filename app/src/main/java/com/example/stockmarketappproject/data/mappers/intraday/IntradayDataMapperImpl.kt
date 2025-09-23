a9d4344b26e93a91750444a40db938539f44d6ae
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-24 01:19:06 +0200

    update intraday mapper; inject formatter;

--- app/src/main/java/com/example/stockmarketappproject/data/mappers/intraday/IntradayDataMapperImpl.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/intraday/IntradayDataMapperImpl.kt
@@ -1,19 +1,33 @@
 package com.example.stockmarketappproject.data.mappers.intraday
 
+import com.example.stockmarketappproject.data.local.entity.CompanyIntradayInfoEntity
 import com.example.stockmarketappproject.data.model.intraday.CompanyIntradayInfoData
 import com.example.stockmarketappproject.data.remote.model.dto.intraday.CompanyIntradayInfoDto
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import com.example.stockmarketappproject.utils.localdatetimeformatter.LocalDateTimeFormatter
 import javax.inject.Inject
 
-class IntradayDataMapperImpl @Inject constructor() : DefaultIntradayDataMapper {
+class IntradayDataMapperImpl @Inject constructor(
+    private val localDateTimeFormatter: LocalDateTimeFormatter
+) : DefaultIntradayDataMapper {
     override fun CompanyIntradayInfoDto.toCompanyIntradayInfoData(): CompanyIntradayInfoData {
-        val pattern = "yyyy-MM-dd HH:mm:ss"
-        val formatter = DateTimeFormatter.ofPattern(pattern)
-        val localDateTime = LocalDateTime.parse(timestamp, formatter)
+        val localDateTime = with(localDateTimeFormatter) {
+            timestamp.toLocalDateTime()
+        }
 
         return CompanyIntradayInfoData(
             timestamp = localDateTime,
+            close = close
+        )
+    }
+
+    override fun CompanyIntradayInfoData.toCompanyIntradayInfoEntity(name: String): CompanyIntradayInfoEntity {
+        val timeStamp = with(localDateTimeFormatter) {
+            timestamp.fromToString()
+        }
+
+        return CompanyIntradayInfoEntity(
+            name = name,
+            timestamp = timeStamp,
             close = close
         )
     }

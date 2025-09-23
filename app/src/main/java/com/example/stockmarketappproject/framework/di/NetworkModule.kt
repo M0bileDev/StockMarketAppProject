c9497fec59986264e89a4c7f56559c8e9715cb57
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-23 13:02:55 +0200

    provide di for api key interceptor; introduce name parameters for providers;

--- app/src/main/java/com/example/stockmarketappproject/framework/di/NetworkModule.kt
+++ app/src/main/java/com/example/stockmarketappproject/framework/di/NetworkModule.kt
@@ -2,6 +2,7 @@ package com.example.stockmarketappproject.framework.di
 
 import com.example.stockmarketappproject.BuildConfig
 import com.example.stockmarketappproject.data.remote.api.StockApi
+import com.example.stockmarketappproject.data.remote.interceptor.ApiKeyInterceptor
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
@@ -14,8 +15,11 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
+import javax.inject.Named
 import javax.inject.Singleton
 
+const val API_KEY_INTERCEPTOR = "API_KEY_INTERCEPTOR"
+const val LOGGING_INTERCEPTOR = "LOGGING_INTERCEPTOR"
 
 @[Module InstallIn(SingletonComponent::class)]
 object NetworkModule {
@@ -43,16 +47,26 @@ object NetworkModule {
     fun provideMoshi(): Moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
 
     @[Provides Singleton]
+    fun provideHttpClient(
+        @Named(LOGGING_INTERCEPTOR) loggingInterceptor: Interceptor,
+        @Named(API_KEY_INTERCEPTOR) apiKeyInterceptor: Interceptor
+    ): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(apiKeyInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @[Provides Singleton Named(LOGGING_INTERCEPTOR)]
     fun provideLoggingInterceptor(): Interceptor {
         return HttpLoggingInterceptor().apply {
             setLevel(HttpLoggingInterceptor.Level.BODY)
         }
     }
 
-    @[Provides Singleton]
-    fun provideHttpClient(
-        interceptor: Interceptor
-    ): OkHttpClient {
-        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    @[Provides Singleton Named(API_KEY_INTERCEPTOR)]
+    fun provideApiKeyInterceptor(): Interceptor {
+        return ApiKeyInterceptor()
     }
 }

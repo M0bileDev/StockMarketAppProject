fceaaa82c1df4723864779d614afd6224c7ad8f2
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-24 19:59:38 +0200

    add event;

--- app/src/main/java/com/example/stockmarketappproject/presentation/model/ViewModelEvents.kt
+++ app/src/main/java/com/example/stockmarketappproject/presentation/model/ViewModelEvents.kt
@@ -2,4 +2,5 @@ package com.example.stockmarketappproject.presentation.model
 
 sealed interface ViewModelEvents {
     object NetworkError : ViewModelEvents
+    object NavigationArgumentError : ViewModelEvents
 }

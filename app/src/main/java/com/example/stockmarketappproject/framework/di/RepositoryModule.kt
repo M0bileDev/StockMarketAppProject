6ff4f1b768fa33f69d2fdf71e870ef8f9544cd2c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-24 14:11:24 +0200

    provide binder for the rest of repos;

--- app/src/main/java/com/example/stockmarketappproject/framework/di/RepositoryModule.kt
+++ app/src/main/java/com/example/stockmarketappproject/framework/di/RepositoryModule.kt
@@ -1,5 +1,9 @@
 package com.example.stockmarketappproject.framework.di
 
+import com.example.stockmarketappproject.data.repository.info.DefaultInfoRepository
+import com.example.stockmarketappproject.data.repository.info.InfoRepositoryImpl
+import com.example.stockmarketappproject.data.repository.intraday.DefaultIntradayRepository
+import com.example.stockmarketappproject.data.repository.intraday.IntradayRepositoryImpl
 import com.example.stockmarketappproject.data.repository.listing.DefaultListingRepository
 import com.example.stockmarketappproject.data.repository.listing.ListingRepositoryImpl
 import dagger.Binds
@@ -13,4 +17,10 @@ abstract class RepositoryModule {
 
     @[Binds Singleton]
     abstract fun bindListingRepository(listingRepositoryImpl: ListingRepositoryImpl): DefaultListingRepository
+
+    @[Binds Singleton]
+    abstract fun bindIntradayRepository(intradayRepositoryImpl: IntradayRepositoryImpl): DefaultIntradayRepository
+
+    @[Binds Singleton]
+    abstract fun bindInfoRepository(infoRepositoryImpl: InfoRepositoryImpl): DefaultInfoRepository
 }

8832ccfb1841976e3a28e8e89ee16fc9786802c4
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-24 14:33:35 +0200

    implement mapper for entity;

--- app/src/main/java/com/example/stockmarketappproject/data/mappers/info/InfoDataMapperImpl.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/info/InfoDataMapperImpl.kt
@@ -1,11 +1,32 @@
 package com.example.stockmarketappproject.data.mappers.info
 
+import com.example.stockmarketappproject.data.local.entity.CompanyInfoEntity
 import com.example.stockmarketappproject.data.model.info.CompanyInfoData
 import com.example.stockmarketappproject.data.remote.model.dto.info.CompanyInfoDto
 import javax.inject.Inject
 
 class InfoDataMapperImpl @Inject constructor() : DefaultInfoDataMapper {
     override fun CompanyInfoDto.toCompanyInfoData(): CompanyInfoData {
+        return CompanyInfoData(
+            symbol,
+            description,
+            name,
+            country,
+            industry
+        )
+    }
+
+    override fun CompanyInfoData.toCompanyInfoEntity(): CompanyInfoEntity {
+        return CompanyInfoEntity(
+            symbol,
+            description,
+            name,
+            country,
+            industry
+        )
+    }
+
+    override fun CompanyInfoEntity.toCompanyInfoData(): CompanyInfoData {
         return CompanyInfoData(
             symbol,
             description,

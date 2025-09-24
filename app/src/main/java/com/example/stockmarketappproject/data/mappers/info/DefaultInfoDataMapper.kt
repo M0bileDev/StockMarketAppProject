933f6de3160d09e8ff86191b3b0cb2c22b79f708
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-24 14:33:23 +0200

    update mapper for entity;

--- app/src/main/java/com/example/stockmarketappproject/data/mappers/info/DefaultInfoDataMapper.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/info/DefaultInfoDataMapper.kt
@@ -1,6 +1,7 @@
 package com.example.stockmarketappproject.data.mappers.info
 
+import com.example.stockmarketappproject.data.local.entity.CompanyInfoEntity
 import com.example.stockmarketappproject.data.model.info.CompanyInfoData
 import com.example.stockmarketappproject.data.remote.model.dto.info.CompanyInfoDto
 
-interface DefaultInfoDataMapper : InfoDataMapper<CompanyInfoDto, CompanyInfoData>
+interface DefaultInfoDataMapper : InfoDataMapper<CompanyInfoDto, CompanyInfoData, CompanyInfoEntity>

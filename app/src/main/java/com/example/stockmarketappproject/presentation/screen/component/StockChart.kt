806ef47c24b5c9015f39bed68360e0261df92224
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-25 01:54:05 +0200

    introduce custom stock chart component;

---
+++ app/src/main/java/com/example/stockmarketappproject/presentation/screen/component/StockChart.kt
@@ -0,0 +1,10 @@
+package com.example.stockmarketappproject.presentation.screen.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun StockChart(modifier: Modifier = Modifier) {
+
+
+}

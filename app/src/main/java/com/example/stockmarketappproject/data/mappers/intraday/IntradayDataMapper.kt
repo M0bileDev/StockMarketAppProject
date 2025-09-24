e184c0e438e97308fdcdee8467a6783307d977ef
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-24 13:59:26 +0200

    add mapper function from entity to data;

--- app/src/main/java/com/example/stockmarketappproject/data/mappers/intraday/IntradayDataMapper.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/intraday/IntradayDataMapper.kt
@@ -7,4 +7,5 @@ import com.example.stockmarketappproject.data.remote.model.dto.DtoModel
 interface IntradayDataMapper<in Dto : DtoModel, Data : DataModel, Entity : EntityModel> {
     fun Dto.toCompanyIntradayInfoData(): Data
     fun Data.toCompanyIntradayInfoEntity(name: String): Entity
+    fun Entity.toCompanyIntradayInfoData(): Data
 }

af1741446a2a8fa062c40b36a8a8fe5edb0c6b46
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-24 14:33:10 +0200

    introduce mapper for entity;

--- app/src/main/java/com/example/stockmarketappproject/data/mappers/info/InfoDataMapper.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/mappers/info/InfoDataMapper.kt
@@ -1,8 +1,11 @@
 package com.example.stockmarketappproject.data.mappers.info
 
+import com.example.stockmarketappproject.data.local.entity.EntityModel
 import com.example.stockmarketappproject.data.model.DataModel
 import com.example.stockmarketappproject.data.remote.model.dto.DtoModel
 
-interface InfoDataMapper<in Dto : DtoModel, out Data : DataModel> {
+interface InfoDataMapper<in Dto : DtoModel, Data : DataModel, Entity : EntityModel> {
     fun Dto.toCompanyInfoData(): Data
+    fun Data.toCompanyInfoEntity(): Entity
+    fun Entity.toCompanyInfoData(): Data
 }

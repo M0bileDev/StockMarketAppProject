58a76c618a76b192c048a2a457a1ee0c19edca65
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-25 23:20:20 +0200

    implement navigation event handle; update view; introduce lifecycle varaible;

--- app/src/main/java/com/example/stockmarketappproject/presentation/screen/companylisting/CompanyListingScreen.kt
+++ app/src/main/java/com/example/stockmarketappproject/presentation/screen/companylisting/CompanyListingScreen.kt
@@ -16,24 +16,45 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.stockmarketappproject.R
 import com.example.stockmarketappproject.presentation.model.listing.CompanyListingEvent
+import com.example.stockmarketappproject.presentation.model.listing.ViewModelListingEvents
 import com.example.stockmarketappproject.presentation.screen.component.CompanyListingItem
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 @Composable
 fun CompanyListingScreen(
     modifier: Modifier = Modifier,
+    onNavigateToCompanyInfo: (String) -> Unit,
     viewModel: CompanyListingViewModel = hiltViewModel<CompanyListingViewModel>()
 ) = with(viewModel) {
 
+    val lifecycle = LocalLifecycleOwner.current
     val companyListingState by state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(lifecycle) {
+        lifecycle.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            event.distinctUntilChanged().collectLatest { event ->
+                when {
+                    event is ViewModelListingEvents.NavigateToCompanyInfo -> {
+                        onNavigateToCompanyInfo(event.name)
+                    }
+                }
+            }
+        }
+    }
 
     with(companyListingState) {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -73,7 +94,7 @@ fun CompanyListingScreen(
                                 modifier = Modifier
                                     .fillMaxWidth()
                                     .clickable {
-                                        // TODO: add logic
+                                        onEvent(CompanyListingEvent.OnNavigate(company.name))
                                     }
                                     .padding(16.dp),
                                 companyListingPresentation = company

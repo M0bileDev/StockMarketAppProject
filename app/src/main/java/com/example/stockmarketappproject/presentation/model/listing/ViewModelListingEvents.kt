e6087d55f8c3375f090390da00a4d79308e6663d
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-25 23:15:42 +0200

    introduce events for listing view model;

---
+++ app/src/main/java/com/example/stockmarketappproject/presentation/model/listing/ViewModelListingEvents.kt
@@ -0,0 +1,8 @@
+package com.example.stockmarketappproject.presentation.model.listing
+
+import com.example.stockmarketappproject.presentation.model.ViewModelEvents
+
+interface ViewModelListingEvents : ViewModelEvents {
+
+    data class NavigateToCompanyInfo(val name: String) : ViewModelListingEvents
+}

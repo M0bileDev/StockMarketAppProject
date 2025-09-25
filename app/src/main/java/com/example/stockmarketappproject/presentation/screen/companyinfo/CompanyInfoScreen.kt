186a7f1fabb4f55558bd0139204ae8875b376062
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-25 15:58:31 +0200

    build screen;

---
+++ app/src/main/java/com/example/stockmarketappproject/presentation/screen/companyinfo/CompanyInfoScreen.kt
@@ -0,0 +1,71 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.stockmarketappproject.presentation.screen.companyinfo
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+fun CompanyInfoScreen(
+    modifier: Modifier = Modifier,
+    companyInfoViewModel: CompanyInfoViewModel = hiltViewModel()
+) = with(companyInfoViewModel) {
+
+    val companyInfoState by companyInfoViewModel.state.collectAsStateWithLifecycle()
+    val scrollable = rememberScrollState()
+
+    with(companyInfoState) {
+        PullToRefreshBox(
+            modifier = modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+            isRefreshing = isRefreshing,
+            onRefresh = {
+                // TODO: support screen events
+            }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(16.dp)
+                    .scrollable(scrollable, orientation = Orientation.Vertical)
+            ) {
+                company?.let { presentation ->
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = presentation.name,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+        }
+    }
+
+
+}

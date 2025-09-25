d28ee375833fd6ed64f77fc333ff8915689d8b71
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-25 21:52:52 +0200

    define destinations;

---
+++ app/src/main/java/com/example/stockmarketappproject/framework/directions/AppDirections.kt
@@ -0,0 +1,9 @@
+package com.example.stockmarketappproject.framework.directions
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object CompanyListing
+
+@Serializable
+data class CompanyInfo(val name: String)

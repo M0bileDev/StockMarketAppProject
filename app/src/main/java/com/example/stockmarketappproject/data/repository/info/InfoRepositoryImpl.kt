49e78d4cf5f32e9f2933df4de983d9983f46828f
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-26 13:28:13 +0200

    update implementation;

--- app/src/main/java/com/example/stockmarketappproject/data/repository/info/InfoRepositoryImpl.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/repository/info/InfoRepositoryImpl.kt
@@ -6,7 +6,6 @@ import com.example.stockmarketappproject.data.model.info.CompanyInfoData
 import com.example.stockmarketappproject.data.remote.api.InfoApi
 import com.example.stockmarketappproject.utils.model.Resource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.transform
 import okio.IOException
 import retrofit2.HttpException
@@ -17,42 +16,53 @@ class InfoRepositoryImpl @Inject constructor(
     private val infoDao: InfoDao,
     private val defaultInfoDataMapper: DefaultInfoDataMapper
 ) : DefaultInfoRepository {
-    override fun getCompanyInfo(name: String): Flow<Resource<CompanyInfoData>> =
+    override fun getCompanyInfo(name: String): Flow<Resource<CompanyInfoData?>> =
         infoDao.getCompanyInfo(name).transform { companyInfoEntity ->
             try {
+
+                if (companyInfoEntity == null) throw IllegalStateException("Data is empty")
+
                 val result = with(defaultInfoDataMapper) {
                     companyInfoEntity.toCompanyInfoData()
                 }
+
                 emit(Resource.Success(result))
+            } catch (ise: IllegalStateException) {
+                ise.printStackTrace()
+                emit(Resource.Error(ise.localizedMessage))
             } catch (e: Exception) {
+                e.printStackTrace()
                 emit(Resource.Error(e.localizedMessage))
             }
         }
 
-    override suspend fun fetchCompanyInfo(name: String): Flow<Resource<CompanyInfoData>> {
-        return flow {
-            try {
-                val response = infoApi.getCompanyInfo(name)
-                if (response == null) throw IllegalStateException("Data is empty")
+    override suspend fun fetchCompanyInfo(name: String): Resource<CompanyInfoData> =
+        try {
+            val response = infoApi.getCompanyInfo(name)
+            if (response == null) throw IllegalStateException("Data is empty")
 
-                val data = with(defaultInfoDataMapper) {
-                    response.toCompanyInfoData()
-                }
-                val result = with(defaultInfoDataMapper) {
-                    data.toCompanyInfoEntity()
-                }
-
-                infoDao.deleteCompanyInfo(name)
-                infoDao.insertCompanyInfo(result)
-
-                emit(Resource.Success(data))
-            } catch (ise: IllegalStateException) {
-                emit(Resource.Error(ise.localizedMessage))
-            } catch (ioe: IOException) {
-                emit(Resource.Error(ioe.localizedMessage))
-            } catch (httpe: HttpException) {
-                emit(Resource.Error(httpe.localizedMessage))
+            val data = with(defaultInfoDataMapper) {
+                response.toCompanyInfoData()
             }
+            val result = with(defaultInfoDataMapper) {
+                data.toCompanyInfoEntity()
+            }
+
+            infoDao.deleteCompanyInfo(name)
+            infoDao.insertCompanyInfo(result)
+
+            Resource.Success(data)
+        } catch (ise: IllegalStateException) {
+            ise.printStackTrace()
+            Resource.Error(ise.localizedMessage)
+        } catch (ioe: IOException) {
+            ioe.printStackTrace()
+            Resource.Error(ioe.localizedMessage)
+        } catch (httpe: HttpException) {
+            httpe.printStackTrace()
+            Resource.Error(httpe.localizedMessage)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Resource.Error(e.localizedMessage)
         }
-    }
 }

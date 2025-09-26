8bb6f8ac7c250d66c038475f8cc3ce64cdea1ce5
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-26 13:38:04 +0200

    add protection against using the wrong thread for execution;

--- app/src/main/java/com/example/stockmarketappproject/data/repository/listing/ListingRepositoryImpl.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/repository/listing/ListingRepositoryImpl.kt
@@ -6,11 +6,14 @@ import com.example.stockmarketappproject.data.model.listing.CompanyListingData
 import com.example.stockmarketappproject.data.parser.listing.DefaultCsvListingParser
 import com.example.stockmarketappproject.data.remote.api.ListingApi
 import com.example.stockmarketappproject.utils.model.Resource
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.withContext
 import okio.IOException
 import retrofit2.HttpException
 import javax.inject.Inject
+import kotlin.printStackTrace
 
 class ListingRepositoryImpl @Inject constructor(
     private val listingApi: ListingApi,
@@ -38,29 +41,32 @@ class ListingRepositoryImpl @Inject constructor(
         }
 
     override suspend fun fetchCompanyListing(): Resource<List<CompanyListingData>> =
-        try {
-            val response = listingApi.getListings()
-            val data = defaultCsvListingParser.parse(response.byteStream())
-            val result = with(defaultListingDataMapper) {
-                data.map { companyListingData -> companyListingData.toCompanyListingEntity() }
-            }
-            if (result.isEmpty()) throw IllegalStateException("Data is empty")
+        withContext(Dispatchers.IO) {
+            try {
+                val response = listingApi.getListings()
+                val data = defaultCsvListingParser.parse(response.byteStream())
+                val result = with(defaultListingDataMapper) {
+                    data.map { companyListingData -> companyListingData.toCompanyListingEntity() }
+                }
+                if (result.isEmpty()) throw IllegalStateException("Data is empty")
 
-            // TODO: think about upsert
-            listingDao.clearCompanyListings()
-            listingDao.insertCompanyListing(result)
-            Resource.Success(data)
-        } catch (ise: IllegalStateException) {
-            ise.printStackTrace()
-            Resource.Error(ise.localizedMessage)
-        } catch (ioe: IOException) {
-            ioe.printStackTrace()
-            Resource.Error(ioe.localizedMessage)
-        } catch (httpe: HttpException) {
-            httpe.printStackTrace()
-            Resource.Error(httpe.localizedMessage)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Resource.Error(e.localizedMessage)
+                // TODO: think about upsert
+                listingDao.clearCompanyListings()
+                listingDao.insertCompanyListing(result)
+                Resource.Success(data)
+            } catch (ise: IllegalStateException) {
+                ise.printStackTrace()
+                Resource.Error(ise.localizedMessage)
+            } catch (ioe: IOException) {
+                ioe.printStackTrace()
+                Resource.Error(ioe.localizedMessage)
+            } catch (httpe: HttpException) {
+                httpe.printStackTrace()
+                Resource.Error(httpe.localizedMessage)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Resource.Error(e.localizedMessage)
+            }
         }
+
 }

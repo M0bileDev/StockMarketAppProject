7d0b7350788a6e0ed1227ce6d502ae5039c8ce2e
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-26 13:29:24 +0200

    update implementation;

--- app/src/main/java/com/example/stockmarketappproject/data/repository/intraday/IntradayRepositoryImpl.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/repository/intraday/IntradayRepositoryImpl.kt
@@ -7,7 +7,6 @@ import com.example.stockmarketappproject.data.parser.intraday.DefaultCsvIntraday
 import com.example.stockmarketappproject.data.remote.api.IntradayApi
 import com.example.stockmarketappproject.utils.model.Resource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.transform
 import okio.IOException
 import retrofit2.HttpException
@@ -26,38 +25,47 @@ class IntradayRepositoryImpl @Inject constructor(
                 val result = with(defaultIntradayDataMapper) {
                     companyIntradayInfoEntities.map { entity -> entity.toCompanyIntradayInfoData() }
                 }
+
+                if (result.isEmpty()) throw IllegalStateException("Data is empty")
+
                 emit(Resource.Success(data = result))
+            } catch (ise: IllegalStateException) {
+                ise.printStackTrace()
+                emit(Resource.Error(ise.localizedMessage))
             } catch (e: Exception) {
+                e.printStackTrace()
                 emit(Resource.Error(e.localizedMessage))
             }
         }
 
-    override fun fetchIntradayInfo(name: String): Flow<Resource<List<CompanyIntradayInfoData>>> {
-        return flow {
-            try {
-                val response = intradayApi.getIntradayInfo(name)
-                val data = defaultCsvIntradayParser.parse(response.byteStream())
-                val result = with(defaultIntradayDataMapper) {
-                    data.map { companyIntradayInfoData ->
-                        companyIntradayInfoData.toCompanyIntradayInfoEntity(
-                            name
-                        )
-                    }
+    override suspend fun fetchIntradayInfo(name: String): Resource<List<CompanyIntradayInfoData>> =
+        try {
+            val response = intradayApi.getIntradayInfo(name)
+            val data = defaultCsvIntradayParser.parse(response.byteStream())
+            val result = with(defaultIntradayDataMapper) {
+                data.map { companyIntradayInfoData ->
+                    companyIntradayInfoData.toCompanyIntradayInfoEntity(
+                        name
+                    )
                 }
-                if (result.isEmpty()) throw IllegalStateException("Data is empty")
-
-                //todo logically when company listing was deleted, intraday info also has to be deleted (all info)
-                intradayDao.deleteCompanyIntradayInfo(name)
-                intradayDao.insertCompanyIntradayInfo(result)
-                emit(Resource.Success(data))
-            } catch (ise: IllegalStateException) {
-                emit(Resource.Error(ise.localizedMessage))
-            } catch (ioe: IOException) {
-                emit(Resource.Error(ioe.localizedMessage))
-            } catch (httpe: HttpException) {
-                emit(Resource.Error(httpe.localizedMessage))
             }
-        }
-    }
+            if (result.isEmpty()) throw IllegalStateException("Data is empty")
 
+            //todo logically when company listing was deleted, intraday info also has to be deleted (all info)
+            intradayDao.deleteCompanyIntradayInfo(name)
+            intradayDao.insertCompanyIntradayInfo(result)
+            Resource.Success(data)
+        } catch (ise: IllegalStateException) {
+            ise.printStackTrace()
+            Resource.Error(ise.localizedMessage)
+        } catch (ioe: IOException) {
+            ioe.printStackTrace()
+            Resource.Error(ioe.localizedMessage)
+        } catch (httpe: HttpException) {
+            httpe.printStackTrace()
+            Resource.Error(httpe.localizedMessage)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Resource.Error(e.localizedMessage)
+        }
 }

1ee6d9d9accfe227c726c6772188de82cf5c1f45
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-27 14:35:56 +0200

    rename mapper to more universal;

--- app/src/main/java/com/example/stockmarketappproject/presentation/mapper/PresentationMapper.kt
+++ app/src/main/java/com/example/stockmarketappproject/presentation/mapper/PresentationMapper.kt
@@ -3,7 +3,7 @@ package com.example.stockmarketappproject.presentation.mapper
 import com.example.stockmarketappproject.data.model.DataModel
 import com.example.stockmarketappproject.presentation.model.PresentationModel
 
-interface StockPresentationMapper<Data : DataModel, Presentation : PresentationModel> {
+interface PresentationMapper<Data : DataModel, Presentation : PresentationModel> {
 
     fun Data.toPresentation(): Presentation
 }

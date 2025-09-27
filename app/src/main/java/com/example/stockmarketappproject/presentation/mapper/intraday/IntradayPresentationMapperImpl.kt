63267a027a45efc35e9012345802138a12594b33
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-27 23:01:10 +0200

    provide di contract;

--- app/src/main/java/com/example/stockmarketappproject/presentation/mapper/intraday/IntradayPresentationMapperImpl.kt
+++ app/src/main/java/com/example/stockmarketappproject/presentation/mapper/intraday/IntradayPresentationMapperImpl.kt
@@ -2,8 +2,9 @@ package com.example.stockmarketappproject.presentation.mapper.intraday
 
 import com.example.stockmarketappproject.data.model.intraday.CompanyIntradayInfoData
 import com.example.stockmarketappproject.presentation.model.intraday.CompanyIntradayInfoPresentation
+import javax.inject.Inject
 
-class IntradayPresentationMapperImpl : IntradayPresentationMapper {
+class IntradayPresentationMapperImpl @Inject constructor(): IntradayPresentationMapper {
     override fun CompanyIntradayInfoData.toPresentation(): CompanyIntradayInfoPresentation {
         return CompanyIntradayInfoPresentation(
             timestamp,

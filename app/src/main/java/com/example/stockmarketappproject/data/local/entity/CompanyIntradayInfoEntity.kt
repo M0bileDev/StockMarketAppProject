093849e81432f82fa276ff84b4481a3b6b7ea20a
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-27 13:19:38 +0200

    change column name;

--- app/src/main/java/com/example/stockmarketappproject/data/local/entity/CompanyIntradayInfoEntity.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/local/entity/CompanyIntradayInfoEntity.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 data class CompanyIntradayInfoEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
-    val name: String,
+    val symbol: String,
     val timestamp: String,
     val close: Double
 ) : EntityModel

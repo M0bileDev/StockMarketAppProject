d650d76f9a73f4fdb692ce6005f5de0b6fdc0b22
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-27 14:32:04 +0200

    remove default mapper;

--- app/src/main/java/com/example/stockmarketappproject/presentation/mapper/DefaultStockPresentationMapper.kt
+++
@@ -1,7 +0,0 @@
-package com.example.stockmarketappproject.presentation.mapper
-
-import com.example.stockmarketappproject.data.model.listing.CompanyListingData
-import com.example.stockmarketappproject.presentation.model.listing.CompanyListingPresentation
-
-interface DefaultStockPresentationMapper :
-    StockPresentationMapper<CompanyListingData, CompanyListingPresentation>

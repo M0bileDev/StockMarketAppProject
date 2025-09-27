1314c2bd949ace3c2ab2c1aaf4a95987ed99780c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-27 14:36:22 +0200

    introduce property for success (not null if result is success);

--- app/src/main/java/com/example/stockmarketappproject/utils/model/Resource.kt
+++ app/src/main/java/com/example/stockmarketappproject/utils/model/Resource.kt
@@ -1,6 +1,6 @@
 package com.example.stockmarketappproject.utils.model
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T?) : Resource<T>(data)
+    class Success<T>(val successData: T) : Resource<T>(data = successData)
     class Error<T>(message: String?, data: T? = null) : Resource<T>(data, message)
 }

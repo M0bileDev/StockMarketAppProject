40a0bda4997905a334b4590a45167158a64364da
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-27 14:33:56 +0200

    change implementation name;

--- app/src/main/java/com/example/stockmarketappproject/presentation/mapper/listing/ListingPresentationMapperImpl.kt
+++ app/src/main/java/com/example/stockmarketappproject/presentation/mapper/listing/ListingPresentationMapperImpl.kt
@@ -1,10 +1,10 @@
-package com.example.stockmarketappproject.presentation.mapper
+package com.example.stockmarketappproject.presentation.mapper.listing
 
 import com.example.stockmarketappproject.data.model.listing.CompanyListingData
 import com.example.stockmarketappproject.presentation.model.listing.CompanyListingPresentation
 import javax.inject.Inject
 
-class StockPresentationMapperImpl @Inject constructor(): DefaultStockPresentationMapper {
+class ListingPresentationMapperImpl @Inject constructor(): ListingPresentationMapper {
 
     override fun CompanyListingData.toPresentation(): CompanyListingPresentation {
         return CompanyListingPresentation(name, symbol, exchange)

efd44efd78a559bb5d368c8792dc7154f96623f7
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-28 03:38:50 +0200

    set contract with clear table provider;

--- app/src/main/java/com/example/stockmarketappproject/data/local/dao/InfoDao.kt
+++ app/src/main/java/com/example/stockmarketappproject/data/local/dao/InfoDao.kt
@@ -3,11 +3,12 @@ package com.example.stockmarketappproject.data.local.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import com.example.stockmarketappproject.data.local.ClearTableProvider
 import com.example.stockmarketappproject.data.local.entity.CompanyInfoEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface InfoDao {
+interface InfoDao : ClearTableProvider  {
 
     @Insert
     suspend fun insertCompanyInfo(
@@ -26,4 +27,9 @@ interface InfoDao {
             """
     )
     fun getCompanyInfo(query: String): Flow<CompanyInfoEntity?>
+
+    @Query("DELETE FROM company_info_entity")
+    suspend fun clearCompanyInfo()
+
+    override suspend fun clearAllTables() = clearCompanyInfo()
 }

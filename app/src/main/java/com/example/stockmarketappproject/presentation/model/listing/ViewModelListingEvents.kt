182fe53443fd9d40615507afef15828c67977b7e
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-28 23:34:37 +0200

    add event;

--- app/src/main/java/com/example/stockmarketappproject/presentation/model/listing/ViewModelListingEvents.kt
+++ app/src/main/java/com/example/stockmarketappproject/presentation/model/listing/ViewModelListingEvents.kt
@@ -5,4 +5,5 @@ import com.example.stockmarketappproject.presentation.model.ViewModelEvents
 interface ViewModelListingEvents : ViewModelEvents {
 
     data class NavigateToCompanyInfo(val name: String) : ViewModelListingEvents
+    data object DismissSnackbar: ViewModelListingEvents
 }

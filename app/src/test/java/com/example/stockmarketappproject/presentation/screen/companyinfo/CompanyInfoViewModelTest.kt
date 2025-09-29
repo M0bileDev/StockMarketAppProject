8b5818c1ebe2adf9f52d7e172a2d8aae5b4e92e9
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-29 20:49:25 +0200

    create test;

--- app/src/test/java/com/example/stockmarketappproject/presentation/screen/companyinfo/CompanyInfoViewModelTest.kt
+++ app/src/test/java/com/example/stockmarketappproject/presentation/screen/companyinfo/CompanyInfoViewModelTest.kt
@@ -3,12 +3,19 @@
 package com.example.stockmarketappproject.presentation.screen.companyinfo
 
 import androidx.lifecycle.SavedStateHandle
+import com.example.stockmarketappproject.data.model.info.CompanyInfoData
+import com.example.stockmarketappproject.data.model.intraday.CompanyIntradayInfoData
 import com.example.stockmarketappproject.data.repository.info.DefaultInfoRepository
 import com.example.stockmarketappproject.data.repository.intraday.DefaultIntradayRepository
 import com.example.stockmarketappproject.presentation.mapper.info.InfoPresentationMapper
 import com.example.stockmarketappproject.presentation.mapper.intraday.IntradayPresentationMapper
+import com.example.stockmarketappproject.presentation.model.ViewModelEvents
+import com.example.stockmarketappproject.presentation.model.info.CompanyInfoPresentation
 import com.example.stockmarketappproject.presentation.model.info.ViewModelInfoEvents
+import com.example.stockmarketappproject.presentation.model.intraday.CompanyIntradayInfoPresentation
 import com.example.stockmarketappproject.utils.dispatcherprovider.DispatcherProvider
+import com.example.stockmarketappproject.utils.model.Resource
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.CoroutineDispatcher
@@ -16,6 +23,8 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
@@ -23,6 +32,7 @@ import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.time.LocalDateTime
 
 class CompanyInfoViewModelTest {
 
@@ -72,5 +82,66 @@ class CompanyInfoViewModelTest {
         mainThreadSurrogate.close()
     }
 
+    @Test
+    fun givenViewModel_whenNavArgIsNotNull_thenInitExecutesPositively() {
+        val mainThreadSurrogate = newSingleThreadContext("UI thread")
+        Dispatchers.setMain(mainThreadSurrogate)
+        var action: ViewModelEvents? = null
 
+        //no deadlock between runBlocking and Dispatchers.Main
+        runBlocking(mainThreadSurrogate) {
+            //given viewModel
+
+            //when current location is null
+            val companyInfoViewModel = createViewModelWithRules {
+                every { savedStateHandle.get<String>("symbol") } returns ""
+                every { companyInfoRepository.getCompanyInfo("") } returns flow {
+                    emit(
+                        Resource.Success(
+                            CompanyInfoData("", "", "", "", "")
+                        )
+                    )
+                }
+                every { intradayRepository.getIntradayInfo("") } returns flow {
+                    emit(
+                        Resource.Success(
+                            listOf(
+                                CompanyIntradayInfoData(LocalDateTime.now(), 0.0)
+                            )
+                        )
+                    )
+                }
+                coEvery { companyInfoRepository.fetchCompanyInfo("") } returns Resource.Success(
+                    CompanyInfoData("", "", "", "", "")
+                )
+                coEvery {
+                    intradayRepository.fetchIntradayInfo("")
+                } returns Resource.Success(
+                    listOf(
+                        CompanyIntradayInfoData(LocalDateTime.now(), 0.0)
+                    )
+                )
+                every { infoPresentationMapper.run { any<CompanyInfoData>().toPresentation() } } returns CompanyInfoPresentation(
+                    "",
+                    "",
+                    "",
+                    "",
+                    ""
+                )
+                every { intradayPresentationMapper.run { any<CompanyIntradayInfoData>().toPresentation() } } returns CompanyIntradayInfoPresentation(
+                    LocalDateTime.now(), 0.0
+                )
+            }
+
+            //then action is NoLocationData
+            val job = launch {
+                action = companyInfoViewModel.event.first()
+            }
+            job.cancel()
+            assertEquals(null, action)
+        }
+
+        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
+        mainThreadSurrogate.close()
+    }
 }

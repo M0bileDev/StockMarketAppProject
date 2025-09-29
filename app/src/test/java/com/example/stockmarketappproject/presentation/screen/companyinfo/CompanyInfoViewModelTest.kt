8140410d542943286351db7dcdeb63681152aa6a
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-29 20:46:33 +0200

    create test class; setup test configuration;

---
+++ app/src/test/java/com/example/stockmarketappproject/presentation/screen/companyinfo/CompanyInfoViewModelTest.kt
@@ -0,0 +1,45 @@
+@file:OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
+
+package com.example.stockmarketappproject.presentation.screen.companyinfo
+
+import androidx.lifecycle.SavedStateHandle
+import com.example.stockmarketappproject.data.repository.info.DefaultInfoRepository
+import com.example.stockmarketappproject.data.repository.intraday.DefaultIntradayRepository
+import com.example.stockmarketappproject.presentation.mapper.info.InfoPresentationMapper
+import com.example.stockmarketappproject.presentation.mapper.intraday.IntradayPresentationMapper
+import com.example.stockmarketappproject.utils.dispatcherprovider.DispatcherProvider
+import io.mockk.mockk
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+
+class CompanyInfoViewModelTest {
+
+    private val savedStateHandle = mockk<SavedStateHandle>()
+    private val companyInfoRepository = mockk<DefaultInfoRepository>()
+    private val intradayRepository = mockk<DefaultIntradayRepository>()
+    private val infoPresentationMapper = mockk<InfoPresentationMapper>()
+    private val intradayPresentationMapper = mockk<IntradayPresentationMapper>()
+    private val dispatcherProvider = object : DispatcherProvider {
+        override val io: CoroutineDispatcher = UnconfinedTestDispatcher()
+        override val main: CoroutineDispatcher = UnconfinedTestDispatcher()
+        override val default: CoroutineDispatcher = UnconfinedTestDispatcher()
+    }
+
+    fun createViewModelWithRules(mockRules: () -> Unit): CompanyInfoViewModel {
+        mockRules()
+        return CompanyInfoViewModel(
+            savedStateHandle,
+            companyInfoRepository,
+            intradayRepository,
+            infoPresentationMapper,
+            intradayPresentationMapper,
+            dispatcherProvider
+        )
+    }
+
+
+
+
+}

e38cccafd8225009feb3ce5b544350f5502afa72
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-29 20:48:13 +0200

    create test;

--- app/src/test/java/com/example/stockmarketappproject/presentation/screen/companyinfo/CompanyInfoViewModelTest.kt
+++ app/src/test/java/com/example/stockmarketappproject/presentation/screen/companyinfo/CompanyInfoViewModelTest.kt
@@ -7,12 +7,22 @@ import com.example.stockmarketappproject.data.repository.info.DefaultInfoReposit
 import com.example.stockmarketappproject.data.repository.intraday.DefaultIntradayRepository
 import com.example.stockmarketappproject.presentation.mapper.info.InfoPresentationMapper
 import com.example.stockmarketappproject.presentation.mapper.intraday.IntradayPresentationMapper
+import com.example.stockmarketappproject.presentation.model.info.ViewModelInfoEvents
 import com.example.stockmarketappproject.utils.dispatcherprovider.DispatcherProvider
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class CompanyInfoViewModelTest {
 
@@ -39,7 +49,28 @@ class CompanyInfoViewModelTest {
         )
     }
 
+    @Test
+    fun givenViewModel_whenNavArgIsNull_thenEventIsNavigationArgumentError() {
+        val mainThreadSurrogate = newSingleThreadContext("UI thread")
+        Dispatchers.setMain(mainThreadSurrogate)
 
+        //no deadlock between runBlocking and Dispatchers.Main
+        runBlocking(mainThreadSurrogate) {
+            //given viewModel
+
+            //when current location is null
+            val companyInfoViewModel = createViewModelWithRules {
+                every { savedStateHandle.get<String>("symbol") } returns null
+            }
+
+            //then action is NoLocationData
+            val action = companyInfoViewModel.event.first()
+            assertEquals(ViewModelInfoEvents.NavigationArgumentError, action)
+        }
+
+        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
+        mainThreadSurrogate.close()
+    }
 
 
 }
